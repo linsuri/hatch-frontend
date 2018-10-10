b3b23d6d757d09e70917047532c5dbcdd3fbdaec
github username <github email address>
2018-10-10 16:23:13 -0400

    .

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,3 +1,5 @@
+import * from '../types'
+
 export const signUp = (email_address, password, first_name, last_name) => {
   return (dispatch) => {
     fetch("http://192.168.2.29:3000/api/v1/users", {
